13b3e4679e2f5da459e07da19a148e43f1eee63c
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2018-09-30 08:32:39 -0700

    Update mini_max_sum.java
    Complete solution.

--- mini_max_sum.java
+++ mini_max_sum.java
@@ -10,6 +10,17 @@ public class Solution {
 
     // Complete the miniMaxSum function below.
     static void miniMaxSum(int[] arr) {
+        long minimumSum = 0;
+        long maximumSum = 0;
+        for (int indexToExclude = 0; indexToExclude < arr.length; indexToExclude++) {
+            long sum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (i != indexToExclude) sum += arr[i];
+            }
+            if (minimumSum == 0 || sum < minimumSum) minimumSum = sum;
+            if (maximumSum == 0 || sum > maximumSum) maximumSum = sum;
+        }
+        System.out.println(minimumSum + " " + maximumSum);
     }
 
     private static final Scanner scanner = new Scanner(System.in);
