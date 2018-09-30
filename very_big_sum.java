03d077a2ab7bc59bbfb5e517d69843359bf8f1b1
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2018-09-29 21:48:47 -0700

    Update very_big_sum.java
    Complete solution.

--- very_big_sum.java
+++ very_big_sum.java
@@ -10,6 +10,11 @@ public class Solution {
 
     // Complete the aVeryBigSum function below.
     static long aVeryBigSum(long[] ar) {
+        long sum = 0;
+        for (long i : ar) {
+            sum += i;
+        }
+        return sum;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
