900a1f2309e2f360ab76c4f9ee230392b2031b72
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2020-06-08 12:47:29 -0700

    Complete solution.

--- interview_recursion/fibonacci/Solution.java
+++ interview_recursion/fibonacci/Solution.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class Solution {
 
     public static int fibonacci(int n) {
-        return 0;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
 
