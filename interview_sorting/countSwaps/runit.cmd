2d76d6ca012084386062e5d256261210dd7580d2
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2020-06-07 22:24:54 -0700

    More sample input after run.

--- interview_sorting/countSwaps/runit.cmd
+++ interview_sorting/countSwaps/runit.cmd
@@ -1,7 +1,9 @@
 javac Solution.java
 java Solution < input1.txt
-type %OUTPUT_PATH%
+rem type %OUTPUT_PATH%
 java Solution < input2.txt
-type %OUTPUT_PATH%
+rem type %OUTPUT_PATH%
 java Solution < input3.txt
-type %OUTPUT_PATH%
+rem type %OUTPUT_PATH%
+java Solution < input4.txt
+rem type %OUTPUT_PATH%
