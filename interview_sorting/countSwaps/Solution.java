2a85fd320d771048ac9fd580b387c5bfb2dac927
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2020-06-07 22:24:37 -0700

    Complete solution.

--- interview_sorting/countSwaps/Solution.java
+++ interview_sorting/countSwaps/Solution.java
@@ -10,6 +10,22 @@ public class Solution {
 
     // Complete the countSwaps function below.
     static void countSwaps(int[] a) {
+        int n = a.length;
+        int numSwaps = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    numSwaps++;
+                }
+            }
+        }
+        System.out.println("Array is sorted in " + numSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n - 1]);
     }
 
     private static final Scanner scanner = new Scanner(System.in);
