a97012ed1afa57ff7458e12a2e05374c72a7f6d7
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2020-06-07 18:51:34 -0700

    New problem.  Also start using a script to run the programs!

---
+++ repeated_string/runit.cmd
@@ -0,0 +1,7 @@
+javac Solution.java
+java Solution < input1.txt
+type ..\..\..\Temp\output.txt
+java Solution < input2.txt
+type ..\..\..\Temp\output.txt
+java Solution < input3.txt
+type ..\..\..\Temp\output.txt
