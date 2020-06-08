dc8369d28c4095c6022a55175ba7b19c7415097f
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2020-06-08 00:03:46 -0700

    New problem.

---
+++ interview_greedy/minimum_absolute_difference/runit.cmd
@@ -0,0 +1,9 @@
+javac Solution.java
+java Solution < input1.txt
+type %OUTPUT_PATH%
+java Solution < input2.txt
+type %OUTPUT_PATH%
+java Solution < input3.txt
+type %OUTPUT_PATH%
+java Solution < input4.txt
+type %OUTPUT_PATH%
