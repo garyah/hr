f66a27ca4b949ac75631a35e177e221f4833d70b
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2020-06-07 21:14:24 -0700

    New problem.

---
+++ interview_hashing/check_magazine/runit.cmd
@@ -0,0 +1,11 @@
+javac Solution.java
+java Solution < input1.txt
+rem type %OUTPUT_PATH%
+java Solution < input2.txt
+rem type %OUTPUT_PATH%
+java Solution < input3.txt
+rem type %OUTPUT_PATH%
+java Solution < input3.txt
+rem type %OUTPUT_PATH%
+java Solution < input4.txt
+rem type %OUTPUT_PATH%
