731a44423b8102e015da3d9520d763873d724709
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2020-06-08 13:13:26 -0700

    New sample inputs after run.

--- interview_linked_lists/insert_node_at_position/runit.cmd
+++ interview_linked_lists/insert_node_at_position/runit.cmd
@@ -3,5 +3,7 @@ java Solution < input1.txt
 type %OUTPUT_PATH%
 java Solution < input2.txt
 type %OUTPUT_PATH%
-rem java Solution < input3.txt
-rem type %OUTPUT_PATH%
+java Solution < input3.txt
+type %OUTPUT_PATH%
+java Solution < input4.txt
+type %OUTPUT_PATH%
