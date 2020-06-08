c0260731b10da58d2d233586c83857b9a331c37e
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2020-06-08 12:39:33 -0700

    New problem.

---
+++ interview_recursion/fibonacci/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static int fibonacci(int n) {
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
+}
